009a393db4d542362afd56290a6f56c726d75ebc
SahilT0 <Sahiltuli16117@gmail.com>
2025-02-14 16:52:35 +0530

    Checking for every column value count

--- Python.py
+++ Python.py
@@ -41,5 +41,22 @@ summary_num = data.describe()
 # Summary of Categorical variables
 pd.set_option("display.max_columns", None) # For all columns display in pycharm
 summary_cat = data.describe(include="object")
-print(summary_cat)
+# print(summary_cat)
+
+# Frequency of each category
+pd.set_option("display.max_rows", None)
+print(data.columns)
+print(data["age"].value_counts())
+print(data["JobType"].value_counts())
+print(data["EdType"].value_counts())
+print(data["maritalstatus"].value_counts())
+print(data["occupation"].value_counts())
+print(data["relationship"].value_counts())
+print(data["race"].value_counts())
+print(data["gender"].value_counts())
+print(data["capitalgain"].value_counts())
+print(data["capitalloss"].value_counts())
+print(data["hoursperweek"].value_counts())
+print(data["nativecountry"].value_counts())
+print(data["SalStat"].value_counts())
 
